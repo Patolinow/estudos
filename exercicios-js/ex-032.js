05f28d1a1c17617c39bd9d1bf59a01638cd80eef
Patolinow <65193403+Patolinow@users.noreply.github.com>
2022-11-30 23:31:26 -0300

    exercicio 32

---
+++ exercicios-js/ex-032.js
@@ -0,0 +1,13 @@
+let techs = ["html", "css", "js"]
+
+techs.push("nodejs")
+
+techs.unshift("sql")
+
+// techs.pop("")
+
+// techs.shift("")
+
+console.log(techs.splice(1,1))
+
+console.log(techs.indexOf("css"))
