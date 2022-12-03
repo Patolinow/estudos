f175f4a357d045cc2e5f25a4ec262b16848dc942
Patolinow <65193403+Patolinow@users.noreply.github.com>
2022-12-03 20:20:54 -0300

    exercicio 36 completo

---
+++ exercicios-js/ex-036.js
@@ -0,0 +1,27 @@
+function maxIntChain(n) {
+  let c = 2
+  let d = n - c
+  let theBiggestOne = 0
+
+  if (n < 5) {
+    theBiggestOne = -1
+  }
+
+  else if (n % 2 != 0) {
+    while (d !== c + 1) {
+      d--
+      c++
+    }
+  }
+
+  else {
+    while (d - 1 !== c + 1) {
+      d--
+      c++
+    }
+  }
+
+  return theBiggestOne = c * d
+}
+
+console.log(maxIntChain(17))
