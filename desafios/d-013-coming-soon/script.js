7da3bfe2f8e164a62d0a4236f16247b9d863a02e
Patolinow <65193403+Patolinow@users.noreply.github.com>
2022-12-07 19:50:25 -0300

    falta terminar o javascript - desafio 13 'em breve'

---
+++ desafios/d-013-coming-soon/script.js
@@ -0,0 +1,9 @@
+let days = 00
+let hours = 00
+let minutes = 00
+let seconds = 00
+
+
+ document.getElementById('times').onclick = function countdown() {
+    document.getElementById('times').innerHTML = '<h2>11</h2> <h2>:</h2> <h2>11</h2> <h2>:</h2> <h2>11</h2> <h2>:</h2> <h2>11</h2>'
+}
