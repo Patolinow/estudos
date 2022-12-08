abefbc631b8e928a6a5b969a276e9c847af10a21
Patolinow <65193403+Patolinow@users.noreply.github.com>
2022-12-08 02:24:50 -0300

    desafio 13  completo, so falta fazer os extras e media queries

--- desafios/d-013-coming-soon/script.js
+++ desafios/d-013-coming-soon/script.js
@@ -1,9 +1,31 @@
-let days = 00
-let hours = 00
-let minutes = 00
-let seconds = 00
+// Conseguindo o tempo que falta até o foguete lançar
+const date = new Date()
 
+let days = 12 - date.getDate()
+let hours = 14 - date.getHours()
+let minutes = 30 - date.getMinutes()
+let seconds = date.getSeconds()
 
- document.getElementById('times').onclick = function countdown() {
-    document.getElementById('times').innerHTML = '<h2>11</h2> <h2>:</h2> <h2>11</h2> <h2>:</h2> <h2>11</h2> <h2>:</h2> <h2>11</h2>'
+// Cronômetro que coloca o tempo no HTML
+function countdown() {
+
+  if ((seconds -= 1) == 0) {
+    seconds = 59
+    minutes--
+  }
+  if (minutes == 0) {
+    minutes = 59
+    hours--
+  }
+  if (hours == 0) {
+    hours == 23
+    days--
+  }
+
+  document.getElementById('times').innerHTML = `<h2>${(days).toLocaleString('pt-br', { minimumIntegerDigits: 2 })}
+: ${(hours).toLocaleString('pt-br', { minimumIntegerDigits: 2 })}
+: ${(minutes).toLocaleString('pt-br', { minimumIntegerDigits: 2 })}
+: ${(seconds).toLocaleString('pt-br', { minimumIntegerDigits: 2 })}</h2>`
 }
+
+const every = setInterval(countdown, 1000)
