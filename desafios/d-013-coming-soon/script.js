5a77e1ecaf554d87240014bc735d5c37bec9beb1
Patolinow <65193403+Patolinow@users.noreply.github.com>
2022-12-10 00:25:56 -0300

    desafio 13 modal implementado, falta media queries

--- desafios/d-013-coming-soon/script.js
+++ desafios/d-013-coming-soon/script.js
@@ -1,5 +1,5 @@
 // Declarando tempo do prazo final
-const endDate = new Date("dec 12, 2022 20:15:00").getTime()
+const endDate = new Date("dec 20, 2022 20:44:00").getTime()
 
 // Cronômetro que coloca o tempo no HTML
 function countdown() {
@@ -12,6 +12,13 @@ function countdown() {
   let minutes = Math.floor(timeLeft % (1000 * 60 * 60) / (1000 * 60))
   let seconds = Math.floor(timeLeft % (1000 * 60) / 1000)
 
+  if (seconds < 0) {
+    days = 0
+    hours = 0
+    minutes = 0
+    seconds = 0
+  }
+
   document.getElementById('times').innerHTML = `<h2>${(days).toLocaleString('pt-br', { minimumIntegerDigits: 2 })}
 : ${(hours).toLocaleString('pt-br', { minimumIntegerDigits: 2 })}
 : ${(minutes).toLocaleString('pt-br', { minimumIntegerDigits: 2 })}
@@ -19,3 +26,16 @@ function countdown() {
 }
 
 const every = setInterval(countdown, 1000)
+
+// Funções para abertura e fechamento do modal
+const enterModal = document.getElementById('enterModal')
+const wrapper = document.getElementById('wrapper')
+const submit = document.querySelector('#submit')
+
+enterModal.onclick = function openModal() {
+  wrapper.style.display = "flex"
+}
+
+submit.onclick = function closeModal() {
+  wrapper.style.display = "none"
+}
