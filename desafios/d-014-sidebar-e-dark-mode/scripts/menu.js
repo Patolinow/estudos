14cce1d11e7c3b6f4ec6adcf544a43b8c51be31c
Patolinow <65193403+Patolinow@users.noreply.github.com>
2022-12-19 03:19:35 -0300

    desafio 14 quase completo

--- desafios/d-014-sidebar-e-dark-mode/scripts/menu.js
+++ desafios/d-014-sidebar-e-dark-mode/scripts/menu.js
@@ -1,11 +1,13 @@
 const menu = document.getElementById("menu")
 const toHide = document.querySelectorAll(".hidden")
 const logout = document.querySelector("footer")
+const nav = document.querySelector("nav")
 
 menu.onclick = function OpenAndClose() {
 toHide.forEach(element => {
   element.classList.toggle("hide")
 })
 
+nav.classList.toggle("expanded")
 logout.classList.toggle("spaced")
 }
