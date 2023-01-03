d7592ea170386240d2a54190bbaf254a1ffa2093
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-01-03 20:00:49 -0300

    coloquei em pratica o que aprendi sobre programação funcional

---
+++ exercicios-js/ex-067.js
@@ -0,0 +1,6 @@
+const _if = (bool, funcTrue, funcFalse) => {
+  bool == true ? funcTrue() : funcFalse()
+}
+
+
+// tentei replciar o teste mas é mt complicado, esse é o exercicio: https://www.codewars.com/kata/54147087d5c2ebe4f1000805/train/javascript
