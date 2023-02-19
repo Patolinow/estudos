ecf29126a6c4662cacca5b433fae3724fd7757f8
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-02-18 23:25:25 -0300

    esqueci de atualizar o javaScript gerado pelo typeScript

--- exercicios-typescript/ex-002/functions.js
+++ exercicios-typescript/ex-002/functions.js
@@ -8,7 +8,35 @@ function greet(person) {
 var doSomething = function (person, age, isFunny) {
     return console.log("Hi ".concat(person, "! even though you're ").concat(age, ", you're really ").concat(isFunny ? "funny" : "unfunny", "!"));
 };
+function rando(num) {
+    if (Math.random() < 0.5) {
+        return num.toString();
+    }
+    return num;
+}
+var add = function (x, y) {
+    return x + y;
+};
+var colors = ["red", "orange", "yellow"];
+colors.map(function (color) {
+    return color.toLocaleUpperCase;
+});
+function printTwice(msg) {
+    console.log(msg);
+    console.log(msg);
+}
+function makeError(msg) {
+    throw new Error(msg);
+}
+function gameLoop() {
+    while (true) {
+        console.log("Running...");
+    }
+}
 console.log(square(9));
 greet();
 greet("fabio");
 doSomething("ChickenFace", 76, false);
+rando(2);
+add(1, 5);
+console.log(printTwice("Hello"));
