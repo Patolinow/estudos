0f7fff82bbe7adf73509d6f2e939bfb5168f0286
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-02-20 20:50:22 -0300

    aprendi sobre tuplas no typescript

---
+++ exercicios-typescript/ex-010/tuples.js
@@ -0,0 +1,3 @@
+var color = [100, 255, 50];
+var goodRes = [200, "Ok!"];
+var responses = [goodRes, [404, "Not found"]];
---
+++ exercicios-typescript/ex-010/tuples.ts
@@ -0,0 +1,6 @@
+let color: [number, number, number] = [100, 255, 50];
+
+type HttpResponse = [number, string];
+let goodRes: HttpResponse = [200, "Ok!"];
+
+const responses: HttpResponse[] = [goodRes,[404,"Not found"]];
