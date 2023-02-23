fcad6f197abc4d93df569c3511307b85ead4e6c3
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-02-22 21:33:55 -0300

    base of todos list made

--- desafios/d-015-to-do-list/dist/index.js
+++ desafios/d-015-to-do-list/dist/index.js
@@ -1,2 +1,47 @@
 "use strict";
-console.log("hello world!!");
+const btn = document.querySelector("button");
+const todoInput = document.getElementById("todo-input");
+const todoForm = document.querySelector("form");
+const todoList = document.querySelector("ul");
+let todoStorage = readTodos();
+todoStorage.forEach(createTodoList);
+function readTodos() {
+    const todosJSON = localStorage.getItem("todos");
+    if (todosJSON !== null) {
+        return JSON.parse(todosJSON);
+    }
+    return [];
+}
+function saveTodos() {
+    localStorage.setItem("todos", JSON.stringify(todoStorage));
+}
+function handleSubmit(e) {
+    e.preventDefault();
+    const thisTodo = {
+        text: todoInput.value,
+        isChecked: false,
+    };
+    createTodoList(thisTodo);
+    todoStorage.push(thisTodo);
+    saveTodos();
+    alert(`${todoInput.value} added to the list!`);
+    todoInput.value = "";
+}
+todoForm.addEventListener("submit", handleSubmit);
+function createTodoList(todo) {
+    const newTodoText = todo.text;
+    const newLi = document.createElement("li");
+    const newCheckbox = document.createElement("input");
+    const newP = document.createElement("p");
+    newCheckbox.type = "checkbox";
+    newCheckbox.addEventListener("change", () => {
+        todo.isChecked = newCheckbox.checked;
+        saveTodos();
+    });
+    if (todo.isChecked === true) {
+        newCheckbox.setAttribute("checked", "");
+    }
+    newP.innerText = newTodoText;
+    newLi.append(newP, newCheckbox);
+    todoList.append(newLi);
+}
--- desafios/d-015-to-do-list/src/index.ts
+++ desafios/d-015-to-do-list/src/index.ts
@@ -1,1 +1,63 @@
-console.log("hello world!!")
+interface Todo {
+  text: string;
+  isChecked: boolean;
+}
+
+const btn = document.querySelector("button")!;
+const todoInput = document.getElementById("todo-input")! as HTMLInputElement;
+const todoForm = document.querySelector("form")!;
+const todoList = document.querySelector("ul")!;
+let todoStorage: Todo[] = readTodos();
+
+todoStorage.forEach(createTodoList);
+
+function readTodos(): Todo[] {
+  const todosJSON = localStorage.getItem("todos");
+  if (todosJSON !== null) {
+    return JSON.parse(todosJSON);
+  }
+  return [];
+}
+
+function saveTodos() {
+  localStorage.setItem("todos", JSON.stringify(todoStorage));
+}
+
+function handleSubmit(e: SubmitEvent): void {
+  e.preventDefault();
+
+  const thisTodo: Todo = {
+    text: todoInput.value,
+    isChecked: false,
+  };
+
+  createTodoList(thisTodo);
+  todoStorage.push(thisTodo);
+  saveTodos()
+
+  alert(`${todoInput.value} added to the list!`);
+  todoInput.value = "";
+}
+
+todoForm.addEventListener("submit", handleSubmit);
+
+function createTodoList(todo: Todo): void {
+  const newTodoText = todo.text;
+  const newLi = document.createElement("li");
+  const newCheckbox = document.createElement("input");
+  const newP = document.createElement("p");
+
+  newCheckbox.type = "checkbox";
+  newCheckbox.addEventListener("change", () => {
+    todo.isChecked = newCheckbox.checked;
+    saveTodos()
+  });
+
+  if (todo.isChecked === true) {
+    newCheckbox.setAttribute("checked", "");
+  }
+  newP.innerText = newTodoText;
+
+  newLi.append(newP, newCheckbox);
+  todoList.append(newLi);
+}
