1f406df3c880f1228646315c78abdda7c1ae69f4
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-03-03 22:12:14 -0300

    aprendi como converter segundos para minutos e horas em typescript

---
+++ exercicios-typescript/ex-023/hoursConverter.ts
@@ -0,0 +1,39 @@
+import assert from "assert";
+
+function humanReadable(totalTimeInSeconds: number): string {
+  if (totalTimeInSeconds >= 360000) {
+    throw new Error("can't handle such a big time");
+  }
+
+  let hours = 0,
+    minutes = 0,
+    seconds = 0;
+
+  for (let runningTime = 0; runningTime < totalTimeInSeconds; runningTime++) {
+    seconds++;
+
+    if (seconds == 60) {
+      minutes++;
+      seconds = 0;
+    }
+
+    if (minutes == 60) {
+      hours++;
+      minutes = 0;
+    }
+  }
+
+  return `${hours.toLocaleString("pt-BR", {
+    minimumIntegerDigits: 2,
+  })}:${minutes.toLocaleString("pt-BR", {
+    minimumIntegerDigits: 2,
+  })}:${seconds.toLocaleString("pt-BR", { 
+    minimumIntegerDigits: 2 })}`;
+}
+
+// Tests
+assert.equal(humanReadable(0), "00:00:00", "humanReadable(0)");
+assert.equal(humanReadable(5), "00:00:05", "humanReadable(5)");
+assert.equal(humanReadable(60), "00:01:00", "humanReadable(60)");
+assert.equal(humanReadable(86399), "23:59:59", "humanReadable(86399)");
+assert.equal(humanReadable(359999), "99:59:59", "humanReadable(359999)");
