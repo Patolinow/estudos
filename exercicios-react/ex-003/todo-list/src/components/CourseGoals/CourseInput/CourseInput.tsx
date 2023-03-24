b9623262277f89acad5efe272ea7be9592b24f96
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-03-23 22:56:22 -0300

    aprendi a usar styled components e css modules

--- exercicios-react/ex-003/todo-list/src/components/CourseGoals/CourseInput/CourseInput.tsx
+++ exercicios-react/ex-003/todo-list/src/components/CourseGoals/CourseInput/CourseInput.tsx
@@ -1,27 +1,37 @@
-import { Ref, useRef } from "react";
+import { Ref, useRef, useState } from "react";
 import Button from "../../UI/Buttom/Buttom";
-import "./CourseInput.css";
+import styles from "./CourseInput.module.css"
 
 interface CourseInputProps {
   onNewInput(goalRef: string): void;
+}
+
+interface InvalidProps {
+  readonly isValid?: boolean;
 }
 
 export default function CourseInput({
   onNewInput,
 }: CourseInputProps): JSX.Element {
   const goalRef = useRef<HTMLInputElement>(null);
+  const [isValid, setValid] = useState(true);
 
   const submitHandler: React.FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
 
     const goal = goalRef.current!.value;
+    if (goal == "") {
+      setValid(false);
+      return;
+    }
+    setValid(true);
     onNewInput(goal);
     goalRef.current!.value = "";
   };
 
   return (
-    <form action="" className="goal-form" onSubmit={submitHandler}>
-      <div className="form-control">
+    <form action="" className={styles['goal-form']} onSubmit={submitHandler}>
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label htmlFor="to-add">Course Goal</label>
         <input type="text" name="to-add" id="to-add" ref={goalRef} />
       </div>
--- exercicios-react/ex-003/todo-list/src/components/UI/Buttom/Buttom.tsx
+++ exercicios-react/ex-003/todo-list/src/components/UI/Buttom/Buttom.tsx
@@ -1,14 +1,43 @@
-import { PropsWithChildren } from "react";
-import "./Buttom.css";
+import  React, { PropsWithChildren } from "react";
+import styles from "./Button.module.css"
 
 interface ButtonProps {
   className?: string;
 }
 
+// const Button = styled.button`
+//   border-radius: 8px;
+//   cursor: pointer;
+//   transition: border-color 0.25s;
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
+
+//   &:focus {
+//     outline: none;
+//   }
+
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+
+//   
+// `;
+
+// export default Button;
+
 export default function Button(
   props: PropsWithChildren<ButtonProps>
 ): JSX.Element {
-  const classes = `button ${props.className}`;
+  const classes = `${styles.button} ${props.className}`;
   return (
     <button type="submit" className={classes}>
       {props.children}
