678d2e599b9fb102399037fc92bc81b96bf619d9
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-03-25 01:32:33 -0300

    💄 - UI items added

---
+++ exercicios-react/ex-004/age-list/src/UI/Button/Button.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  font-size: 16px;
+  padding: 8px 12px;
+  background-color: #E81123;
+  border-radius: 4px;
+  border: none;
+  transition: 200ms;
+
+  &:hover {
+    filter: brightness(130%);
+  }
+`
+
+export default Button
