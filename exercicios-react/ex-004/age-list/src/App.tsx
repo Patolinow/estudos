a6fac04c3846e61190e2b882b9d9112d3348141f
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-03-26 20:50:01 -0300

    🚧 - basic inputs and dummy list added

--- exercicios-react/ex-004/age-list/src/App.tsx
+++ exercicios-react/ex-004/age-list/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import User from "./interfaces/User";
+import UserList from "./components/UserList/UserList";
+import UserInput from "./components/UserInputs/UserInput";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const dummyUsers: User[] = [
+    { id: 1, username: "Max", age: 31 },
+    { id: 2, username: "Diego", age: 19 },
+    { id: 3, username: "Dominicoicoloko 'Donssssssss'", age: 1 },
+  ];
+
+  const [users, setUsers] = useState(dummyUsers);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <UserInput />
+      <UserList users={users} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
