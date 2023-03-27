efeec7dce7b4e99f7da7a4e3639b3b4e28d98648
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-03-27 19:05:20 -0300

    💄 - updated styles to not need the color-scheme

--- exercicios-react/ex-004/age-list/src/UI/Button/Button.tsx
+++ exercicios-react/ex-004/age-list/src/UI/Button/Button.tsx
@@ -1,11 +1,16 @@
 import styled from "styled-components";
 
 const Button = styled.button`
+  color: white;
   font-size: 16px;
+  font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+  font-weight: 600;
+
   padding: 8px 12px;
   background-color: #E81123;
   border-radius: 4px;
   border: none;
+
   cursor: pointer;
   transition: 200ms;
 
