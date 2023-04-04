bb7a6663cb140cef48817aa715e3d0f5eecba3cf
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-04-04 19:13:37 -0300

    ✨ - added FoodsMenu

---
+++ desafios/d-015-food-order-app/food-app/src/components/Main/FoodsMenu/FoodsMenuItem/FoodsMenuItem.tsx
@@ -0,0 +1,22 @@
+import styles from "./FoodsMenuItem.module.css";
+import FoodsMenuInput from "./FoodsMenuInput/FoodsMenuInput";
+import Meal from "../../../../interfaces/Meal";
+
+interface FoodsMenuItemProps {
+  meal: Meal;
+}
+
+const FoodsMenuItem = ({meal}: FoodsMenuItemProps): JSX.Element => {
+  return (
+    <li className={styles.meal} key={meal.id}>
+      <div>
+        <h3>{meal.name}</h3>
+        <p className={styles.description}>{meal.description}</p>
+        <p className={styles.price}>${meal.price}</p>
+      </div>
+      <FoodsMenuInput />
+    </li>
+  );
+};
+
+export default FoodsMenuItem;
---
+++ desafios/d-015-food-order-app/food-app/src/components/Main/FoodsMenu/FoodsMenuList/FoodsMenuList.tsx
@@ -0,0 +1,18 @@
+import Card from "../../../UI/Card/Card"
+import styles from "./FoodsMenuList.module.css"
+import FoodsMenuItem from "../FoodsMenuItem/FoodsMenuItem"
+import Meal from "../../../../interfaces/Meal"
+
+interface FoodsMenuListProps {
+  meals: Meal[]
+}
+
+const FoodsMenuList = ({meals}:FoodsMenuListProps):JSX.Element => {
+return (<Card className={styles.meals}>
+  <ul>
+    {meals.map((meal) => <FoodsMenuItem meal={meal}/>)}
+  </ul>
+</Card>)
+}
+
+export default FoodsMenuList
