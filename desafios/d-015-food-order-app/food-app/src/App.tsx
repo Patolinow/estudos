9cfc2b1fb548ce2ed4bf04f4b55c6b9a207a5c2a
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-04-04 19:08:37 -0300

    🚧 - added a type to meals and passing it as a props

--- desafios/d-015-food-order-app/food-app/src/App.tsx
+++ desafios/d-015-food-order-app/food-app/src/App.tsx
@@ -1,41 +1,50 @@
-import './App.css'
-import Header from './components/Header/Header';
-import Main from './components/Main/Main';
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Meal from "./interfaces/Meal";
 
 function App() {
-  const DUMMY_MEALS = [
+  const DUMMY_MEALS: Meal[] = [
     {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
+      id: "m1",
+      name: "Sushi",
+      description: "Finest fish and veggies",
       price: 22.99,
     },
     {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
+      id: "m2",
+      name: "Schnitzel",
+      description: "A german specialty!",
       price: 16.5,
     },
     {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
+      id: "m3",
+      name: "Barbecue Burger",
+      description: "American, raw, meaty",
       price: 12.99,
     },
     {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
+      id: "m4",
+      name: "Green Bowl",
+      description: "Healthy...and green...",
       price: 18.99,
     },
+    {
+      id: "m5",
+      name: "Mozzarella Cheese Pizza",
+      description: "Blessed by Gyro Zeppeli",
+      price: 18.66,
+    },
   ];
+  const [meals, setMeals] = useState(DUMMY_MEALS);
 
   return (
     <div className="App">
-      <Header/>
-      <Main/>
+      <Header />
+      <Main meals={meals} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
