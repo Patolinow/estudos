b502c29633fe7229f6da83ed73a304d15efc29d8
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-04-05 15:23:55 -0300

    🐛 - Fixed onClick and onSubmit doesn't working

--- desafios/d-015-food-order-app/food-app/src/components/UI/Button/Button.tsx
+++ desafios/d-015-food-order-app/food-app/src/components/UI/Button/Button.tsx
@@ -2,7 +2,7 @@ import { ButtonHTMLAttributes, PropsWithChildren } from "react"
 import styles from "./Button.module.css"
 
 const Button = (props:PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>):JSX.Element => {
-  return <button type={props.type} className={`${styles.button} ${props.className}`}>{props.children}</button>
+  return <button type={props.type} className={`${styles.button} ${props.className}`} onClick={props.onClick} onSubmit={props.onSubmit}>{props.children}</button>
 }
 
 export default Button
