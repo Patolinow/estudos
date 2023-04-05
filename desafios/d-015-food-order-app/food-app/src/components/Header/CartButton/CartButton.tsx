1a772e7263e5c566b38e6162b2dbcd540165d5fc
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-04-05 15:44:37 -0300

    🚧 - Added a call to the porta

--- desafios/d-015-food-order-app/food-app/src/components/Header/CartButton/CartButton.tsx
+++ desafios/d-015-food-order-app/food-app/src/components/Header/CartButton/CartButton.tsx
@@ -1,14 +1,25 @@
 import Button from "../../UI/Button/Button";
 import CartIcon from "../../../assets/CartIcon.svg";
 import styles from "./CartButton.module.css"
+import CartModal from "../../CartModal/CartModal";
+import { useState } from "react";
 
 const CartButton = (): JSX.Element => {
-  return (
-    <Button className={`${styles["cart-button"]} ${styles.bump}`}>
+  const [cartState, setCartState] = useState(false)
+
+  const cartHandler = () => {
+    setCartState(true)
+  }
+
+  return (<>
+    {cartState && <CartModal/>}
+
+    <Button className={`${styles["cart-button"]} ${styles.bump}`} onClick={cartHandler}>
       <img src={CartIcon} className={styles["cart-icon"]} /> 
       <p>Your Cart</p> 
       <div className={styles.badge}>5</div>
     </Button>
+    </>
   );
 };
 
