0721a0c9bb3dd33ca6d84628bb98516b585b851c
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-04-05 15:22:04 -0300

    💬 - Updated Tagline text

--- desafios/d-015-food-order-app/food-app/src/components/Main/Tagline/Tagline.tsx
+++ desafios/d-015-food-order-app/food-app/src/components/Main/Tagline/Tagline.tsx
@@ -3,7 +3,7 @@ import styles from "./Tagline.module.css"
 const Tagline = ():JSX.Element => {
   return (
     <section className={styles.summary}>
-      <h2>Delicious Food, Delivered To You</h2>
+      <h2>Delicious Food Delivered For You!</h2>
       <p>
         Choose your favorite meal from our broad selection of available meals
         and enjoy a delicious lunch or dinner at home. All our meals are cooked with high-quality ingredients, just-in-time and
