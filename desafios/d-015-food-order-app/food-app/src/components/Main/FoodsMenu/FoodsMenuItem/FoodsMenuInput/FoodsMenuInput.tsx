850a5af60f750c8ef7ab8a4b0b17cd96cc778212
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-04-11 21:03:31 -0300

    🚧 - Added a way to collect the inputs and send to localStorage

--- desafios/d-015-food-order-app/food-app/src/components/Main/FoodsMenu/FoodsMenuItem/FoodsMenuInput/FoodsMenuInput.tsx
+++ desafios/d-015-food-order-app/food-app/src/components/Main/FoodsMenu/FoodsMenuItem/FoodsMenuInput/FoodsMenuInput.tsx
@@ -1,14 +1,51 @@
-import styles from "./FoodsMenuInput.module.css"
-import Button from "../../../../UI/Button/Button"
+import styles from "./FoodsMenuInput.module.css";
+import Button from "../../../../UI/Button/Button";
+import { useContext, useRef } from "react";
+import NewMeal from "../../../../../interfaces/NewMeal";
+import CartContext from "../../../../contexts/CartContext";
 
-const FoodsMenuInput = ():JSX.Element => {
-return <form className={styles.form}>
-  <div className={styles.input}>
-    <label htmlFor="numInput">Amout</label>
-     <input type="number" name="numInput" min={1} max={99} maxLength={2} id="numInput" defaultValue={1} />
-  </div>
-  <Button>+ Add</Button>
-</form>
+interface InputProps {
+  mealInfos: { id: string; name: string; price: number };
 }
 
-export default FoodsMenuInput
+const FoodsMenuInput = ({ mealInfos }: InputProps): JSX.Element => {
+  const cartContext = useContext(CartContext)
+  const amountRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler: React.FormEventHandler = (e) => {
+    e.preventDefault();
+
+    const HandledAmount = Number(amountRef.current!.value);
+    console.log(HandledAmount)
+
+    const newMeal: NewMeal = {
+      id: mealInfos.id,
+      name: mealInfos.name,
+      price: mealInfos.price,
+      amount: HandledAmount,
+    };
+
+    cartContext.onNewMeal(newMeal)
+    console.log(localStorage.getItem("mealsList"))
+  };
+
+  return (
+    <form className={styles.form} onSubmit={submitHandler}>
+      <div className={styles.input}>
+        <label htmlFor="numInput">Amout</label>
+        <input
+          ref={amountRef}
+          type="number"
+          name="numInput"
+          min={1}
+          max={99}
+          id="numInput"
+          defaultValue={1}
+        />
+      </div>
+      <Button>+ Add</Button>
+    </form>
+  );
+};
+
+export default FoodsMenuInput;
