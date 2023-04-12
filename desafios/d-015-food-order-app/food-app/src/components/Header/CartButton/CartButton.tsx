d0da7c519a446bc27ed47c893d28b5dcf899fb60
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-04-11 21:00:02 -0300

    🚧 - Added a external count to the button

--- desafios/d-015-food-order-app/food-app/src/components/Header/CartButton/CartButton.tsx
+++ desafios/d-015-food-order-app/food-app/src/components/Header/CartButton/CartButton.tsx
@@ -2,22 +2,29 @@ import Button from "../../UI/Button/Button";
 import CartIcon from "../../../assets/CartIcon.svg";
 import styles from "./CartButton.module.css"
 import CartModal from "../../CartModal/CartModal";
-import { useState } from "react";
+import CartContext from "../../contexts/CartContext";
+import { useState, useContext } from "react";
 
 const CartButton = (): JSX.Element => {
   const [cartState, setCartState] = useState(false)
+  const cartContext = useContext(CartContext)
 
-  const cartHandler = () => {
+
+  const openCartHandler = ():void => {
     setCartState(true)
   }
 
-  return (<>
-    {cartState && <CartModal/>}
+  const closeCartHandler = ():void => {
+    setCartState(false)
+  }
 
-    <Button className={`${styles["cart-button"]} ${styles.bump}`} onClick={cartHandler}>
+  return (<>
+    {cartState && <CartModal onClose={closeCartHandler}/>}
+
+    <Button className={`${styles["cart-button"]} ${styles.bump}`} onClick={openCartHandler}>
       <img src={CartIcon} className={styles["cart-icon"]} /> 
       <p>Your Cart</p> 
-      <div className={styles.badge}>5</div>
+      <div className={styles.badge}>{cartContext.totalAmount}</div>
     </Button>
     </>
   );
