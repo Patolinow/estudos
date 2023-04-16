856b2fdee2629a8b86165fc5117999a30c56fae7
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-04-16 15:18:25 -0300

    🚚 - Changed where the portal of the modal pops up

--- desafios/d-015-food-order-app/food-app/src/main.tsx
+++ desafios/d-015-food-order-app/food-app/src/main.tsx
@@ -7,6 +7,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <CartContextProvider>
+    <div id="modal-root"></div>
     <App />
     </CartContextProvider>
   </React.StrictMode>,
