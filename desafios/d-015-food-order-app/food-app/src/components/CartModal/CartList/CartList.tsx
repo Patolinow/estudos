bfe5ce4e88fc73a4864c3ce90390cb6ea03a7a07
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-04-16 16:56:35 -0300

    ✨ - Added the total amount dynamically

--- desafios/d-015-food-order-app/food-app/src/components/CartModal/CartList/CartList.tsx
+++ desafios/d-015-food-order-app/food-app/src/components/CartModal/CartList/CartList.tsx
@@ -2,18 +2,23 @@ import styles from "./CartList.module.css";
 import Button from "../../UI/Button/Button";
 import CartItem from "../CartItem/CartItem";
 import CartContext from "../../contexts/CartContext";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 
 interface CartListProps {
-  onClose():void
+  onClose(): void;
 }
 // Corrigir total quando tem scrollbar
-const CartList = ({onClose}:CartListProps): JSX.Element => {
-  const meals = useContext(CartContext).meals
+const CartList = ({ onClose }: CartListProps): JSX.Element => {
+  const meals = useContext(CartContext).meals;
+  const [finalValue, setFinalValue] = useState(0);
 
+  useEffect(() => {
+    const calculateFinalValue = meals.reduce((total, meal) => total + meal.amount * meal.price, 0);
+    setFinalValue(calculateFinalValue);
+  }, [meals]);
 
   const closeHandler = () => {
-    onClose()
+    onClose();
   };
 
   const orderHandler = () => {
@@ -24,14 +29,14 @@ const CartList = ({onClose}:CartListProps): JSX.Element => {
   return (
     <div className={styles["cart-items"]}>
       <ul>
-        {meals.map((meal) =>
-        <CartItem meal={meal} key={meal.id}/>
-        )}
+        {meals.map((meal) => (
+          <CartItem meal={meal} key={meal.id} />
+        ))}
       </ul>
 
       <div className={styles.total}>
         <h1>Total Amount</h1>
-        <h2>$55.99</h2>
+        <h2>${finalValue}</h2>
       </div>
 
       <div className={styles.actions}>
