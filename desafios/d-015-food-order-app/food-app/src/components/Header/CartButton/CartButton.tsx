6839998d0e0941a5b958fbdfcce36b22ab408803
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-04-18 13:23:45 -0300

    ✨ - Added a animation that runs everytime the list of meals is updated

--- desafios/d-015-food-order-app/food-app/src/components/Header/CartButton/CartButton.tsx
+++ desafios/d-015-food-order-app/food-app/src/components/Header/CartButton/CartButton.tsx
@@ -3,11 +3,24 @@ import CartIcon from "../../../assets/CartIcon.svg";
 import styles from "./CartButton.module.css"
 import CartModal from "../../CartModal/CartModal";
 import CartContext from "../../contexts/CartContext";
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 
 const CartButton = (): JSX.Element => {
   const [cartState, setCartState] = useState(false)
+  const [buttonAnimation, setButtonAnimation] = useState(false)
   const { totalAmount } = useContext(CartContext)
+  const buttonClass = `${styles["cart-button"]} ${buttonAnimation ? styles.bump : ""}`
+
+  useEffect(() => {
+    setButtonAnimation(true)
+    const timer = setTimeout(() => {
+      setButtonAnimation(false)
+    }, 300)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [totalAmount])
 
   const openCartHandler = ():void => {
     setCartState(true)
@@ -21,7 +34,7 @@ const CartButton = (): JSX.Element => {
     <>
       {cartState && <CartModal onClose={closeCartHandler}/>}
 
-      <Button className={`${styles["cart-button"]} ${styles.bump}`} onClick={openCartHandler}>
+      <Button className={buttonClass} onClick={openCartHandler}>
         <img src={CartIcon} className={styles["cart-icon"]} /> 
         <p>Your Cart</p> 
         <div className={styles.badge}>{totalAmount}</div>
