d4cfd9edf7800716afa80cf95ce461d7a6d1ddc4
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-04-17 21:38:59 -0300

    🚚 - Exported CartContextProvider from another component

--- desafios/d-015-food-order-app/food-app/src/main.tsx
+++ desafios/d-015-food-order-app/food-app/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { CartContextProvider } from './components/contexts/CartContext'
+import CartContextProvider from './components/contexts/CartProvider'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
