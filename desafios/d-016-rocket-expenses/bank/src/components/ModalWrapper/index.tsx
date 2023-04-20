1bc8e0d90c09e813fa3043ef5f511e3f43199c34
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-04-20 18:53:06 -0300

    💄 - Updated bg-color to match better

--- desafios/d-016-rocket-expenses/bank/src/components/ModalWrapper/index.tsx
+++ desafios/d-016-rocket-expenses/bank/src/components/ModalWrapper/index.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react"
 
 export const ModalWrapper = (props:PropsWithChildren<React.HTMLAttributes<HTMLDivElement>>):JSX.Element => {
 return (
-  <div className="fixed top-0 left-0 w-full h-full z-20 bg-black/70" onClick={props.onClick}>
+  <div className="fixed top-0 left-0 w-full h-full z-20 bg-black/80" onClick={props.onClick}>
     {props.children}
   </div>
 )
