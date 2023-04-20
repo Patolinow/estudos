9c67aae73283e4232c929be5b6a512fd3f2936f0
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-04-20 19:04:45 -0300

    💄 - Updated the UI of some fields from ModalForm

--- desafios/d-016-rocket-expenses/bank/src/components/ModalForm/index.tsx
+++ desafios/d-016-rocket-expenses/bank/src/components/ModalForm/index.tsx
@@ -1,43 +1,78 @@
-export const ModalForm = (): JSX.Element => {
-  const submitHandler = () => {};
+import iconClose from "../../assets/icons/iconClose.svg";
+import iconIncome from "../../assets/icons/iconIncome.svg";
+import iconOutcome from "../../assets/icons/iconOutcome.svg";
+import { Input } from "../Input";
+import { RadioLabel } from "../RadioLabel";
+
+interface ModalFormProps {
+  openCloseHandler():void
+}
+
+export const ModalForm = ({openCloseHandler}:ModalFormProps): JSX.Element => {
+  const submitHandler:React.FormEventHandler = (e) => {
+    e.preventDefault()
+    console.log("sent")
+  };
   return (
     <form
-      className=" text-white w-11/12 max-w-lg h-[33rem] fixed bg-dark-gray z-30 bottom-1/2 left-1/2 translate-y-1/2 -translate-x-1/2 py-10 px-12  rounded-md max-md:bottom-0 max-md:translate-y-0 max-md:rounded-t-3xl max-md:w-full"
+      className=" text-white w-11/12 max-w-lg h-[33rem] fixed bg-dark-gray z-30 bottom-1/2 left-1/2 translate-y-1/2 -translate-x-1/2 py-10 px-12 shadow-2xl rounded-md max-md:bottom-0 max-md:translate-y-0 max-md:rounded-t-3xl max-md:w-full"
       onSubmit={submitHandler}
     >
-      <fieldset className="flex flex-col">
-        <legend className="text-2xl mb-8 font-bold">Nova transação</legend>
+      {" "}
+      <img
+        src={iconClose}
+        alt="Fechar"
+        className="cursor-pointer hover:brightness-125 duration-200 fixed top-6 right-6"
+        onClick={openCloseHandler}
+      />
+      <fieldset className="flex flex-col group-[rounded-md]:">
+        <legend className="text-2xl font-bold mb-8">Nova transação</legend>
 
-        <input
-          type="text"
-          name="descricao"
-          id="descricao"
-          placeholder="Descrição"
-          className="text-white bg-black p-4 rounded-md placeholder:text-gray"
-        />
+        <Input type="text" name="description" id="description" placeholder="Descrição" />
 
-        <input
-          type="number"
-          name="valor"
-          id="valor"
-          placeholder="Valor (15,90 => 1590)"
-          className="my-4 text-white bg-black p-4 rounded-md placeholder:text-gray"
-        />
+        <Input type="number" name="value" id="value" placeholder="Valor" className="my-4" />
 
-        <input
-          type="text"
-          name="categoria"
-          id="categoria"
-          placeholder="Categoria"
-          className="text-white bg-black p-4 rounded-md placeholder:text-gray"
-        />
+        <Input type="text" name="category" id="category" placeholder="Categoria" />
 
-        <fieldset className="mt-6 mb-10">
-          <input type="radio" name="type" id="income" />
-          <input type="radio" name="type" id="outcome" />
+        <fieldset className="mt-6 mb-10 flex gap-4">
+          <Input
+            type="radio"
+            name="type"
+            id="income"
+            className="peer/income"
+            defaultChecked
+            hidden
+            
+          />
+          <RadioLabel
+            htmlFor="income"
+            className="peer-checked/income:bg-light-green/20"
+          >
+            <img src={iconIncome} />
+            <p>Income</p>
+          </RadioLabel>
+
+          <input
+            type="radio"
+            name="type"
+            id="outcome"
+            hidden
+            className="peer/outcome"
+            
+          />
+          <RadioLabel
+            htmlFor="outcome"
+            className="peer-checked/outcome:bg-red/20"
+          >
+            <img src={iconOutcome} />
+            <p>Outcome</p>
+          </RadioLabel>
         </fieldset>
 
-        <button type="submit" className="p-4 bg-light-green rounded-md hover:bg-dark-green duration-300">
+        <button
+          type="submit"
+          className="p-4 bg-light-green rounded-md hover:bg-dark-green duration-300"
+        >
           Cadastrar
         </button>
       </fieldset>
