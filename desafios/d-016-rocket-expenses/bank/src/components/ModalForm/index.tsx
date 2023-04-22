caefd91f98e2d7376bbfa09ac842ed6194f58bd6
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-04-21 23:37:38 -0300

    ✨ - Added a useRef, added new attributes and updated literals

--- desafios/d-016-rocket-expenses/bank/src/components/ModalForm/index.tsx
+++ desafios/d-016-rocket-expenses/bank/src/components/ModalForm/index.tsx
@@ -1,24 +1,44 @@
+import React, { useRef, useImperativeHandle, forwardRef } from "react";
 import iconClose from "../../assets/icons/iconClose.svg";
 import iconIncome from "../../assets/icons/iconIncome.svg";
 import iconOutcome from "../../assets/icons/iconOutcome.svg";
 import { Input } from "../Input";
 import { RadioLabel } from "../RadioLabel";
+import ITableInputProps from "../../interfaces/ITableInputProps";
 
 interface ModalFormProps {
-  openCloseHandler():void
+  openCloseHandler(): void;
 }
 
-export const ModalForm = ({openCloseHandler}:ModalFormProps): JSX.Element => {
-  const submitHandler:React.FormEventHandler = (e) => {
-    e.preventDefault()
-    console.log("sent")
+export const ModalForm = ({ openCloseHandler }: ModalFormProps): JSX.Element => {
+  
+  const allRefs = {
+    title: useRef() as React.MutableRefObject<HTMLInputElement>,
+    value: useRef() as React.MutableRefObject<HTMLInputElement>,
+    subject: useRef() as React.MutableRefObject<HTMLInputElement>,
+    income: useRef() as React.MutableRefObject<HTMLInputElement>,
+    outcome: useRef() as React.MutableRefObject<HTMLInputElement>
+  } ;
+
+  const submitHandler: React.FormEventHandler = (e) => {
+    e.preventDefault();
+
+    const refs: ITableInputProps = {
+      title: allRefs.title.current.value,
+      subject: allRefs.subject.current.value,
+      value: Number(allRefs.value.current.value) * 100,
+      type: allRefs.income.current.checked ? "income" : "outcome",
+      date: new Date().toLocaleDateString(),
+    };
+
+    console.log(refs);
   };
+
   return (
     <form
-      className=" text-white w-11/12 max-w-lg h-[33rem] fixed bg-dark-gray z-30 bottom-1/2 left-1/2 translate-y-1/2 -translate-x-1/2 py-10 px-12 shadow-2xl rounded-md max-md:bottom-0 max-md:translate-y-0 max-md:rounded-t-3xl max-md:w-full"
+      className=" text-white w-11/12 max-w-lg h-[33rem] fixed bg-dark-gray z-30 bottom-1/2 left-1/2 translate-y-1/2 -translate-x-1/2 py-10 px-12 rounded-md max-md:bottom-0 max-md:translate-y-0 max-md:rounded-t-3xl max-md:w-full"
       onSubmit={submitHandler}
     >
-      {" "}
       <img
         src={iconClose}
         alt="Fechar"
@@ -28,11 +48,32 @@ export const ModalForm = ({openCloseHandler}:ModalFormProps): JSX.Element => {
       <fieldset className="flex flex-col group-[rounded-md]:">
         <legend className="text-2xl font-bold mb-8">Nova transação</legend>
 
-        <Input type="text" name="description" id="description" placeholder="Descrição" />
+        <Input
+          type="text"
+          name="description"
+          id="description"
+          placeholder="Descrição"
+          autoComplete="off"
+          ref={allRefs.title}
+        />
 
-        <Input type="number" name="value" id="value" placeholder="Valor" className="my-4" />
+        <Input
+          type="number"
+          name="value"
+          id="value"
+          placeholder="Valor"
+          className="my-4"
+          ref={allRefs.value}
+        />
 
-        <Input type="text" name="category" id="category" placeholder="Categoria" />
+        <Input
+          type="text"
+          name="category"
+          id="category"
+          placeholder="Categoria"
+          autoComplete="off"
+          ref={allRefs.subject}
+        />
 
         <fieldset className="mt-6 mb-10 flex gap-4">
           <Input
@@ -40,32 +81,28 @@ export const ModalForm = ({openCloseHandler}:ModalFormProps): JSX.Element => {
             name="type"
             id="income"
             className="peer/income"
+            value="income"
+            ref={allRefs.income}
             defaultChecked
             hidden
-            
           />
-          <RadioLabel
-            htmlFor="income"
-            className="peer-checked/income:bg-light-green/20"
-          >
+          <RadioLabel htmlFor="income" className="peer-checked/income:bg-light-green/20">
             <img src={iconIncome} />
-            <p>Income</p>
+            <p>Receita</p>
           </RadioLabel>
 
           <input
             type="radio"
             name="type"
             id="outcome"
-            hidden
+            value="outcome"
             className="peer/outcome"
-            
+            ref={allRefs.outcome}
+            hidden
           />
-          <RadioLabel
-            htmlFor="outcome"
-            className="peer-checked/outcome:bg-red/20"
-          >
+          <RadioLabel htmlFor="outcome" className="peer-checked/outcome:bg-red/20">
             <img src={iconOutcome} />
-            <p>Outcome</p>
+            <p>Despesa</p>
           </RadioLabel>
         </fieldset>
 
