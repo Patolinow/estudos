13da933bcd38da49f37f21b3bc50bc681029826c
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-04-21 23:11:01 -0300

    ✨ - Added dummy values

--- desafios/d-016-rocket-expenses/bank/src/components/TotalOutcomesAndIncomes/index.tsx
+++ desafios/d-016-rocket-expenses/bank/src/components/TotalOutcomesAndIncomes/index.tsx
@@ -1,13 +1,18 @@
-import { Card } from "../Card"
+import { Card } from "../Card";
 
 export const TotalOutcomesAndIncomes = () => {
+  const total = {
+    incomes: 450000 + 50000,
+    outcomes: 5000 + 7000,
+    balance: 450000 + 50000 - (5000 + 7000),
+  };
   return (
     <div className="flex justify-center -mt-16">
-        <div className="grid grid-cols-3 gap-8 max-w-5xl w-full">
-          <Card type="income" value={0} />
-          <Card type="outcome" value={0} />
-          <Card type="total" value={0} />
-        </div>
+      <div className="grid grid-cols-3 gap-8 max-w-5xl w-full">
+        <Card type="income" value={total.incomes} />
+        <Card type="outcome" value={total.outcomes} />
+        <Card type="total" value={total.balance} />
       </div>
-  )
-}
+    </div>
+  );
+};
