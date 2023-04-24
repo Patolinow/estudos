e8fe63a40de0f1baa5c092d896a9a82d549003cf
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-04-23 22:41:30 -0300

    ✨ - Added a way to close the modal by clicking out

--- desafios/d-015-food-order-app/food-app/src/components/CartModal/CartModal.tsx
+++ desafios/d-015-food-order-app/food-app/src/components/CartModal/CartModal.tsx
@@ -6,19 +6,27 @@ interface CartModalProps {
   onClose():void
 }
 
+const ModalWrapper = ({onClose}:CartModalProps) => {
+  return (
+    <div className={styles.backdrop} onClick={onClose}>
+
+    </div>
+  )
+}
+
 const PortalCartModal = ({onClose}:CartModalProps): JSX.Element => {
   return (
-    <div className={styles.backdrop}>
       <div className={styles.modal}>
         <CartList onClose={onClose}/>
       </div>
-    </div>
   );
 };
 
 const CartModal = ({onClose}:CartModalProps): JSX.Element => {
+  const modalRoot = document.getElementById("modal-root")!
   return (<>
-  {ReactDOM.createPortal(<PortalCartModal onClose={onClose}/>, document.getElementById("modal-root")!)}
+  {ReactDOM.createPortal(<PortalCartModal onClose={onClose}/>, modalRoot)}
+  {ReactDOM.createPortal(<ModalWrapper onClose={onClose}/>, modalRoot)}
   </>)
 };
 
