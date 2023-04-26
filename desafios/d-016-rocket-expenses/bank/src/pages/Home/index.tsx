968a5a7a5d7cf95b8489ef90281413f84a59aa54
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-04-26 01:46:12 -0300

    🔥 - Removed dummy data

--- desafios/d-016-rocket-expenses/bank/src/pages/Home/index.tsx
+++ desafios/d-016-rocket-expenses/bank/src/pages/Home/index.tsx
@@ -1,29 +1,12 @@
-import ITableInputProps from "../../interfaces/ITableInputProps";
 import { Header } from "../../components/Header";
 import { Main } from "../../components/Main";
 
 export const Home = (): JSX.Element => {
-  const data: ITableInputProps[] = [
-    {
-      type: "income",
-      title: "Salário",
-      subject: "Salário",
-      value: 450000,
-      date: "01/04/2023",
-    },
-    {
-      type: "outcome",
-      title: "Rodízio de pizza",
-      subject: "Alimentação",
-      value: 3000,
-      date: "01/04/2023",
-    },
-  ];
 
   return (
     <div className="h-screen bg-dark-gray text-white">
       <Header />
-      <Main data={data} />
+      <Main />
     </div>
   );
 };
