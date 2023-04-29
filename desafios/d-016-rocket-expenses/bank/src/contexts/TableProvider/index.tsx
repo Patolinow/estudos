b09cf1b4cf5fdb653676d2186864319122da1cdb
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-04-29 02:44:39 -0300

    🐛 - Changed useReducer to a useEffect

--- desafios/d-016-rocket-expenses/bank/src/contexts/TableProvider/index.tsx
+++ desafios/d-016-rocket-expenses/bank/src/contexts/TableProvider/index.tsx
@@ -1,33 +1,8 @@
 import { TableContext } from "../TableContext";
 import { localStorageManager } from "../../utils/localStorageManager";
-import { PropsWithChildren, useReducer, useState } from "react";
+import { PropsWithChildren, useEffect, useReducer, useState } from "react";
 import ITableInputProps from "../../interfaces/ITableInputProps";
 import TableInputValues from "../../interfaces/TableInputValues";
-
-interface Action {
-  type: "income" | "outcome";
-  newTableInput: ITableInputProps;
-}
-
-const reducer = (state: TableInputValues, action: Action) => {
-  switch (action.type) {
-    case "income":
-      state.incomes += action.newTableInput.value;
-      break;
-
-    case "outcome":
-      state.outcomes += action.newTableInput.value;
-      break;
-
-    default:
-      throw new Error(`Type ${action.type} doesn't exist`);
-  }
-
-  state.balance = state.incomes - state.outcomes;
-  state.totalItems++;
-
-  return state;
-};
 
 export const TableContextProvider = (props: PropsWithChildren): JSX.Element => {
   const tableKey = "tableKey";
@@ -37,51 +12,43 @@ export const TableContextProvider = (props: PropsWithChildren): JSX.Element => {
   }
 
   const [storageTableInputs, setStorageTableInputs] = useState(localStorageManager.get(tableKey));
-  const tableInputValuesInitialState: TableInputValues = {
+  const [displayTableInputs, setDisplayTableInputs] = useState(storageTableInputs);
+  const [tableInputValues, setTableInputValues] = useState<TableInputValues>({
     totalItems: 0,
+    balance: 0,
     incomes: 0,
     outcomes: 0,
-    balance: 0,
-  };
-  const [displayTableInputs, setDisplayTableInputs] = useState(storageTableInputs);
+  });
 
-  const initialStateFunc = (initialValues: TableInputValues) => {
-    const currentTableInputs = storageTableInputs;
-
-    const incomes = currentTableInputs.filter((tableInputValue) => {
-      return tableInputValue.type == "income";
+  useEffect(() => {
+    const currentTableInputs = [...storageTableInputs];
+    const currentTableInputsIncomes = currentTableInputs.filter((currentTableInput) => {
+      return currentTableInput.type === "income";
     });
-    initialValues.incomes = incomes.reduce((totalIncomes, actualIncome) => {
-      return totalIncomes + actualIncome.value;
-    }, 0);
-
-    const outcomes = currentTableInputs.filter((tableInputValue) => {
-      return tableInputValue.type == "outcome";
+    const currentTableInputsOutcomes = currentTableInputs.filter((currentTableInput) => {
+      return currentTableInput.type === "outcome";
     });
-    initialValues.outcomes = outcomes.reduce((totalOutcomes, actualOutcome) => {
-      return totalOutcomes + actualOutcome.value;
-    }, 0);
+    const finalIncome = currentTableInputsIncomes.reduce((total, actual) => total + actual.value, 0)
+    const finalOutcome = currentTableInputsOutcomes.reduce((total, actual) => total + actual.value, 0)
 
-    initialValues.balance = initialValues.incomes - initialValues.outcomes;
-    initialValues.totalItems = storageTableInputs.length;
-
-    return initialValues;
-  };
-
-  const [tableInputValues, tableInputValuesDispatch] = useReducer(
-    reducer,
-    tableInputValuesInitialState,
-    initialStateFunc
-  );
+    setTableInputValues(() => {
+      const newTableInputValues: TableInputValues = {
+        totalItems: currentTableInputs.length,
+        incomes: finalIncome,
+        outcomes: finalOutcome,
+        balance: finalIncome - finalOutcome
+      };
+      return newTableInputValues;
+    });
+  }, [storageTableInputs]);
 
   const newTableInputHandler = (newTableInput: ITableInputProps) => {
-    const currentTableInputs = [...storageTableInputs];
+    const allTableInputs = [newTableInput, ...storageTableInputs];
 
-    setStorageTableInputs((prevStorageTableInputs) => [...prevStorageTableInputs, newTableInput]);
-    setDisplayTableInputs((prevDisplayTableInputs) => [...prevDisplayTableInputs, newTableInput]);
+    setStorageTableInputs(() => allTableInputs);
+    setDisplayTableInputs(() => allTableInputs);
 
-    localStorageManager.set(tableKey, [...currentTableInputs, newTableInput]);
-    tableInputValuesDispatch({ type: newTableInput.type, newTableInput: newTableInput });
+    localStorageManager.set(tableKey, allTableInputs);
   };
 
   const searchHandler = (searchTerm: string) => {
@@ -104,7 +71,7 @@ export const TableContextProvider = (props: PropsWithChildren): JSX.Element => {
 
     setStorageTableInputs(() => filteredCurrentTableInputs);
     setDisplayTableInputs(() => filteredCurrentTableInputs);
-    localStorageManager.set(tableKey, filteredCurrentTableInputs)
+    localStorageManager.set(tableKey, filteredCurrentTableInputs);
   };
 
   return (
