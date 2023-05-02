8503b45b6deb3a57284c886a179fd31e3ae0af5b
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-05-02 00:07:28 -0300

    📱 - Added new items to mobile version

--- desafios/d-016-rocket-expenses/bank/src/components/Main/index.tsx
+++ desafios/d-016-rocket-expenses/bank/src/components/Main/index.tsx
@@ -1,11 +1,20 @@
 import Table from "../Table";
 import { TotalOutcomesAndIncomes } from "../TotalOutcomesAndIncomes";
 import { SearchForm } from "../SearchForm";
+import { useContext } from "react";
+import { TableContext } from "../../contexts/TableContext";
 
 export const Main = () => {
+  const totalItems = useContext(TableContext).totalItems;
+
   return (
-    <main className="w-11/12 max-w-5xl mx-auto">
-      <TotalOutcomesAndIncomes/>
+    <main>
+      <TotalOutcomesAndIncomes />
+
+      <div className="flex justify-between mt-4 mb-2 md:hidden w-11/12 max-w-5xl mx-auto">
+        <p className="text-light-gray text-lg">Transações</p>
+        <p className="text-gray">{totalItems} itens</p>
+      </div>
 
       <SearchForm />
 
