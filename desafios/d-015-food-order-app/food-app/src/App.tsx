3463b43eafc352be2b3b072bc91fb35c38f3ae7a
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-05-17 23:34:21 -0300

    🔥 - Removed dummy meals and component drilling

--- desafios/d-015-food-order-app/food-app/src/App.tsx
+++ desafios/d-015-food-order-app/food-app/src/App.tsx
@@ -1,50 +1,15 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
-import Meal from "./interfaces/Meal";
-import { useState } from "react";
 import CartContextProvider from "./components/contexts/CartProvider";
 
 function App() {
-  const DUMMY_MEALS: Meal[] = [
-    {
-      id: "m1",
-      name: "Sushi",
-      description: "Finest fish and veggies",
-      price: 22.99,
-    },
-    {
-      id: "m2",
-      name: "Schnitzel",
-      description: "A german specialty!",
-      price: 16.5,
-    },
-    {
-      id: "m3",
-      name: "Barbecue Burger",
-      description: "American, raw, meaty",
-      price: 12.99,
-    },
-    {
-      id: "m4",
-      name: "Green Bowl",
-      description: "Healthy...and green...",
-      price: 18.99,
-    },
-    {
-      id: "m5",
-      name: "Mozzarella Cheese Pizza",
-      description: "Blessed by Gyro Zeppeli",
-      price: 18.66,
-    },
-  ];
-  const [meals, setMeals] = useState(DUMMY_MEALS);
 
   return (  
     <CartContextProvider>
     <div className="App">
       <Header />
-      <Main meals={meals} />
+      <Main/>
     </div>
     </CartContextProvider>
   );
--- desafios/d-015-food-order-app/food-app/src/components/Main/Main.tsx
+++ desafios/d-015-food-order-app/food-app/src/components/Main/Main.tsx
@@ -1,20 +1,15 @@
 import styles from "./Main.module.css";
 import Tagline from "./Tagline/Tagline";
 import FoodsMenuList from "./FoodsMenu/FoodsMenuList/FoodsMenuList";
-import Meal from "../../interfaces/Meal";
 
-interface MainProps {
-  meals: Meal[]
-}
-
-const Main = ({meals}:MainProps):JSX.Element => {
+const Main = ():JSX.Element => {
   return (
     <main className={styles.main}>
       <div className={`${styles["main-image"]}`}/>
 
       <Tagline/>
 
-      <FoodsMenuList meals={meals}/>
+      <FoodsMenuList/>
     </main>
   );
 };
