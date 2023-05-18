0b89ce42d59a9577b5195b4d08cdfde67ba69aff
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-05-17 23:31:45 -0300

    ✨ - Added a way to get data from firebase api

--- desafios/d-015-food-order-app/food-app/src/components/Main/FoodsMenu/FoodsMenuList/FoodsMenuList.tsx
+++ desafios/d-015-food-order-app/food-app/src/components/Main/FoodsMenu/FoodsMenuList/FoodsMenuList.tsx
@@ -2,16 +2,36 @@ import Card from "../../../UI/Card/Card"
 import styles from "./FoodsMenuList.module.css"
 import FoodsMenuItem from "../FoodsMenuItem/FoodsMenuItem"
 import Meal from "../../../../interfaces/Meal"
+import useFetch from "../../../../hooks/use-fetch"
+import loadingSvg from "../../../../assets/loading.svg"
 
-interface FoodsMenuListProps {
-  meals: Meal[]
+// colocar td aki e fazer requisição aki
+const FoodsMenuList = ():JSX.Element => {
+const url = "https://max-http-requests-default-rtdb.firebaseio.com/meals.json"
+const mealsRequest = useFetch({url: url, requestMethod: "GET"})
+const meals: Meal[] = []
+
+for (const key in mealsRequest.requestedData) {
+  if (Object.prototype.hasOwnProperty.call(mealsRequest.requestedData, key)) {
+
+    const mealRequested:Meal = {
+      id: key,
+      name: mealsRequest.requestedData[key].name,
+      description: mealsRequest.requestedData[key].description,
+      price: mealsRequest.requestedData[key].price
+    }
+    meals.push(mealRequested)
+  }
 }
 
-const FoodsMenuList = ({meals}:FoodsMenuListProps):JSX.Element => {
+
+const mealsLoaded =  <ul> {meals.map((meal) => <FoodsMenuItem meal={meal} key={meal.id}/>)}</ul>
+const loading = mealsRequest.isLoading ? <img className={styles.notMeals} src={loadingSvg} alt="Loading..." /> : undefined
+const error = mealsRequest.errorMessage ? <p className={styles.notMeals}>{mealsRequest.errorMessage}</p> : undefined
+
+
 return (<Card className={styles.meals}>
-  <ul>
-    {meals.map((meal) => <FoodsMenuItem meal={meal} key={meal.id}/>)}
-  </ul>
+ {loading || error || mealsLoaded}
 </Card>)
 }
 
