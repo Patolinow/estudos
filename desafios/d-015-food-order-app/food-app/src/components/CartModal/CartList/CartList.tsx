537b24c1b41426f2f9c021e483ab72aa727d575c
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-05-21 00:30:50 -0300

    ⚡️ - Improved some handlers and changed html structure

--- desafios/d-015-food-order-app/food-app/src/components/CartModal/CartList/CartList.tsx
+++ desafios/d-015-food-order-app/food-app/src/components/CartModal/CartList/CartList.tsx
@@ -6,29 +6,20 @@ import { useContext, useEffect, useState } from "react";
 
 interface CartListProps {
   onClose(): void;
+  onProceedReturn: () => void;
 }
 // Corrigir total quando tem scrollbar
-const CartList = ({ onClose }: CartListProps): JSX.Element => {
+const CartList = ({ onClose, onProceedReturn }: CartListProps): JSX.Element => {
   const cartContext = useContext(CartContext);
-  const meals = cartContext.meals;
   const finalValue = cartContext.finalValue.toLocaleString("en-US", {maximumFractionDigits: 2, minimumFractionDigits: 2});
   const decreaseHandler = cartContext.onDecrease;
   const increaseHandler = cartContext.onIncrease;
   const resetHandler = cartContext.onReset;
 
-  const closeHandler = () => {
-    onClose();
-  };
-
-  const orderHandler = () => {
-    // Adicionar useEffect para pedir apenas 1 vez
-    console.log("ordering...");
-  };
-
   return (
     <div className={styles["cart-items"]}>
       <ul>
-        {meals.map((meal) => (
+        {cartContext.meals.map((meal) => (
           <CartItem
             key={meal.id}
             meal={meal}
@@ -38,23 +29,22 @@ const CartList = ({ onClose }: CartListProps): JSX.Element => {
         ))}
       </ul>
 
-      <div className={styles.total}>
-        <h1>Total Amount</h1>
-        <h2>${finalValue}</h2>
-      </div>
-
-      <div className={styles.actions}>
-        <Button className={styles.reset} onClick={resetHandler}>
-          Clear
-        </Button>
-
-        <Button className={styles.close} onClick={closeHandler}>
-          Close
-        </Button>
-
-        <Button className={styles.order} onClick={orderHandler}>
-          Order
-        </Button>
+      <div className={styles["total-and-actions"]}>
+        <div className={styles.total}>
+          <h1>Total Amount</h1>
+          <h2>${finalValue}</h2>
+        </div>
+        <div className={styles.actions}>
+          <Button className={styles.reset} onClick={resetHandler}>
+            Clear
+          </Button>
+          <Button className={styles.close} onClick={onClose}>
+            Close
+          </Button>
+          <Button className={styles.order} onClick={onProceedReturn}>
+            Order
+          </Button>
+        </div>
       </div>
     </div>
   );
