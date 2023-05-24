c6408ab58677d10d94704960d93ced9887243297
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-05-23 23:51:03 -0300

    🐛 - Fixed infinity calls with fixed useFetch

--- desafios/d-015-food-order-app/food-app/src/components/Main/FoodsMenu/FoodsMenuList/FoodsMenuList.tsx
+++ desafios/d-015-food-order-app/food-app/src/components/Main/FoodsMenu/FoodsMenuList/FoodsMenuList.tsx
@@ -1,38 +1,60 @@
-import Card from "../../../UI/Card/Card"
-import styles from "./FoodsMenuList.module.css"
-import FoodsMenuItem from "../FoodsMenuItem/FoodsMenuItem"
-import Meal from "../../../../interfaces/Meal"
-import useFetch from "../../../../hooks/use-fetch"
-import loadingSvg from "../../../../assets/loading.svg"
+import Card from "../../../UI/Card/Card";
+import styles from "./FoodsMenuList.module.css";
+import FoodsMenuItem from "../FoodsMenuItem/FoodsMenuItem";
+import Meal from "../../../../interfaces/Meal";
+import useFetch from "../../../../hooks/use-fetch";
+import loadingSvg from "../../../../assets/loading.svg";
+import { useEffect } from "react";
 
-// colocar td aki e fazer requisição aki
-const FoodsMenuList = ():JSX.Element => {
-const url = "https://max-http-requests-default-rtdb.firebaseio.com/meals.json"
-const mealsRequest = useFetch({url: url, requestMethod: "GET"})
-const meals: Meal[] = []
+const FoodsMenuList = (): JSX.Element => {
+  const url = "https://max-http-requests-default-rtdb.firebaseio.com/meals.json";
+  const { sendRequest, isLoading, errorMessage, requestedData } = useFetch();
 
-for (const key in mealsRequest.requestedData) {
-  if (Object.prototype.hasOwnProperty.call(mealsRequest.requestedData, key)) {
+  useEffect(() => {
+    sendRequest({ url: url, requestMethod: "GET" });
+  }, [sendRequest]);
 
-    const mealRequested:Meal = {
-      id: key,
-      name: mealsRequest.requestedData[key].name,
-      description: mealsRequest.requestedData[key].description,
-      price: mealsRequest.requestedData[key].price
-    }
-    meals.push(mealRequested)
+  if (isLoading) {
+    return (
+      <Card className={styles.meals}>
+        <img className={styles.notMeals} src={loadingSvg} alt="Loading..." />
+      </Card>
+    );
   }
-}
 
+  if (errorMessage) {
+    return (
+      <Card className={styles.meals}>
+        <p className={styles.notMeals}>{errorMessage}</p>
+      </Card>
+    );
+  }
 
-const mealsLoaded =  <ul> {meals.map((meal) => <FoodsMenuItem meal={meal} key={meal.id}/>)}</ul>
-const loading = mealsRequest.isLoading ? <img className={styles.notMeals} src={loadingSvg} alt="Loading..." /> : undefined
-const error = mealsRequest.errorMessage ? <p className={styles.notMeals}>{mealsRequest.errorMessage}</p> : undefined
+    const meals: Meal[] = [];
 
+    for (const key in requestedData) {
+      if (Object.prototype.hasOwnProperty.call(requestedData, key)) {
+        const mealRequested: Meal = {
+          id: key,
+          name: requestedData[key].name,
+          description: requestedData[key].description,
+          price: requestedData[key].price,
+        };
+        meals.push(mealRequested);
+      }
+    }
+  
+  
 
-return (<Card className={styles.meals}>
- {loading || error || mealsLoaded}
-</Card>)
-}
+  return (
+    <Card className={styles.meals}>
+      <ul>
+        {meals.map((meal) => (
+          <FoodsMenuItem meal={meal} key={meal.id} />
+        ))}
+      </ul>
+    </Card>
+  );
+};
 
-export default FoodsMenuList
+export default FoodsMenuList;
