1b379ff69fe3602157f5d76b7de1cd2d8a09e271
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-05-30 23:39:17 -0300

    ✨ - Added redux store

---
+++ exercicios-react/ex-015/redux-revision/src/store/index.tsx
@@ -0,0 +1,8 @@
+import {configureStore} from "@reduxjs/toolkit"
+import cartReducer from "./reducer/CartSlice"
+
+const store = configureStore({reducer: {cart: cartReducer}})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+export default store
