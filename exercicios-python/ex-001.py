f8aeb97506fedc23a0c2a495bdf658cf2357a9af
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-06-10 21:44:11 -0300

    primeiro exercício em python

---
+++ exercicios-python/ex-001.py
@@ -0,0 +1,21 @@
+#1
+x = 0.0
+y = 18
+
+print(type(x))
+print(type(y))
+
+z = x + y
+print(z)
+print(type(z))
+
+#2
+a = "valor"
+#print(type(A))
+
+
+#5 e 6
+altura = float(input("digite o valor da altura do triângulo: "))
+base = float(input("agora digite o valor da base do triângulo: "))
+
+print(f"a área deste triângulo é {altura * base / 2}")
