ff9afb6d89a90877ea7847a56e272d8e9e86bfd8
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-07-04 00:12:53 -0300

    aprendi como usar o useSpring

--- exercicios-react/ex-022/learning-animations/src/App.tsx
+++ exercicios-react/ex-022/learning-animations/src/App.tsx
@@ -2,9 +2,28 @@ import Modal from "./components/Modal/Modal";
 import List from "./components/List/List";
 import "./App.css";
 import { useState } from "react";
+import { animated, useTransition, useSpring } from "@react-spring/web";
 
 function App() {
+  const [springs, api] = useSpring(() => ({from: {x: -50, opacity: 0}, config: {duration: 500}}))
   const [isOpen, setIsOpen] = useState(false);
+  const [showBlock, setShowBlock] = useState(true);
+  // const blockTransitions = useTransition(showBlock, transitionStyle);
+  const blockToggleHandler = () => {
+    setShowBlock((prevShowBlock) => !prevShowBlock);
+
+    if (showBlock) {
+      api.start({
+        to: {x: 500, opacity: 1}
+      })
+    }
+    else {
+      api.start({
+        to: {x: -500, opacity: 0}
+      })
+    }
+    
+  };
   const modalToggleHandler = () => {
     setIsOpen((prevIsOpen) => !prevIsOpen);
   };
@@ -12,6 +31,20 @@ function App() {
   return (
     <div className="App">
       <div id="modal-root"></div>
+      <button className="Button" onClick={blockToggleHandler}>
+        Toggle
+      </button>
+          <animated.div
+            style={{
+              backgroundColor: "red",
+              width: 100,
+              height: 100,
+              margin: "auto",
+              ...springs
+            }}
+          />
+      
+      <br />
       <h1>React Animations</h1>
       {isOpen ? <Modal closed={modalToggleHandler}/> : undefined}
       <button className="Button" onClick={modalToggleHandler}>
