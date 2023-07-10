6d9aee93b912b5b8cdb232e53d09b74017e51db8
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-07-10 20:05:58 -0300

    aprendi como usar useTransition do react spring em vários elementos de uma lista

--- exercicios-react/ex-022/learning-animations/src/components/List/List.tsx
+++ exercicios-react/ex-022/learning-animations/src/components/List/List.tsx
@@ -1,33 +1,40 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import './List.css';
+import "./List.css";
+import { animated, useTransition } from "@react-spring/web";
 
 const List = () => {
-    const [items, setItems] = useState([1,2,3])
-    const addItemHandler = () => {
-        setItems(prevItems => [...prevItems, prevItems.length + 1])
-    }
+  const [items, setItems] = useState([1, 2, 3]);
+  const itemsWithTransition = useTransition(items,{
+    keys: item => item,
+    from: { y: 0, opacity: 1 },
+    leave: { y: 20, opacity: 0 },
+  })
+  const addItemHandler = () => {
+    setItems((prevItems) => [...prevItems, prevItems.length + 1]);
+  };
 
-    const removeItemHandler = (item: number) => {
-        setItems(prevItems => prevItems.filter(prevItem => prevItem !== item))
-    }
+  const removeItemHandler = (item: number) => {
+    setItems((prevItems) => prevItems.filter((prevItem) => prevItem !== item));
+  };
 
-    const listItems = items.map( (item, index) => (
-        <li 
-            key={index}
-            className="ListItem" 
-            onClick={() => removeItemHandler(item)}>{item}</li>
-    ) );
+  const listItems = itemsWithTransition((styles, item) => (
+    
+        <animated.li key={Math.random().toString()} className="ListItem" style={styles}  onClick={() => removeItemHandler(item)}>
+          {item}
+        </animated.li>
+    
+  ));
 
-    return (
-        <div>
-            <button className="Button" onClick={addItemHandler}>Add Item</button>
-            <p>Click on the Item to Remove.</p>
-            <ul className="List">
-                {listItems}
-            </ul>
-        </div>
-    );
-}
+  return (
+    <div>
+      <button className="Button" onClick={addItemHandler}>
+        Add Item
+      </button>
+      <p>Click on the Item to Remove.</p>
+      <ul className="List">{listItems}</ul>
+    </div>
+  );
+};
 
 export default List;
