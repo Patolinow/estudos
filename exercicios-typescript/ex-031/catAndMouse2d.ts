a79b9acba4c3b7bf2240ef4ab0a84fd0935403a6
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-07-26 21:17:20 -0300

    aprendi como fazer um graph search através de uma adjacency matrix

--- exercicios-typescript/ex-031/catAndMouse2d.ts
+++ exercicios-typescript/ex-031/catAndMouse2d.ts
@@ -1,14 +1,93 @@
 import { expect } from "@jest/globals";
 
-type CatMouseReturn = "Caught!" | "Escaped!" | "boring without two animals";
+type CatMouseReturn = "Caught!" | "Escaped!" | "boring without two animals" | undefined;
+type MapMatrix = string[][]
+interface ICoordinates {
+  cat: number[];
+  mouse: number[];
+}
 
 function catMouse(map: string, moves: number): CatMouseReturn {
-  const mapArray = map.split("\n")
-  const mapMatrix = mapArray.map(mapRow =>  mapRow.split(""))
+  const cat = "C"
+  const mouse = "m"
+  const allMapItems = map.split("")
 
-  console.log(mapMatrix)
-  return "boring without two animals";
+  if (!allMapItems.includes(cat) || !allMapItems.includes(mouse)) {
+    return "boring without two animals"
+  }
+
+  const mapArray = map.split("\n")
+  const mapMatrix: MapMatrix = mapArray.map(mapRow =>  mapRow.split(""))
+  const coordinates: ICoordinates = findCatAndMouse(mapMatrix, cat, mouse)
+  const totalMovesToMouse = searchRouteToMouse(coordinates)
+
+  if (totalMovesToMouse <= moves) {
+    console.log(`caught with ${totalMovesToMouse} moves! :D`)
+    return "Caught!"
+  }
+
+  else {
+    console.log(`escaped with ${totalMovesToMouse} moves :(`)
+    return "Escaped!"
+  }
+  
+  function findCatAndMouse(mapMatrix: MapMatrix, cat: string, mouse: string){
+    const coordinates: ICoordinates = {cat: [], mouse: []}
+
+    mapMatrix.forEach((mapRow, mapRowIndex) => {
+      mapRow.forEach((mapItem, mapCollumnIndex) => {
+        if (mapItem === cat) {
+          coordinates.cat = [mapRowIndex, mapCollumnIndex]
+        }
+
+        if (mapItem === mouse){
+          coordinates.mouse = [mapRowIndex, mapCollumnIndex]
+        }
+      })
+    })
+    return coordinates
+  }
+
+  function searchRouteToMouse( coordinates: ICoordinates): number {
+    const reachedY = coordinates.cat[0] === coordinates.mouse[0]
+    const reachedX = coordinates.cat[1] === coordinates.mouse[1]
+    const moveDown = coordinates.cat[0] < coordinates.mouse[0] && !reachedY
+    const moveUp = coordinates.cat[0] > coordinates.mouse[0] && !reachedY
+    const moveLeft = coordinates.cat[1] > coordinates.mouse[1] && !reachedX
+    const moveRight = coordinates.cat[1] < coordinates.mouse[1] && !reachedX
+
+    if (moveDown) {
+      coordinates.cat[0]++
+      return 1 + searchRouteToMouse(coordinates)
+    }
+    if (moveUp) {
+      coordinates.cat[0]--
+      return 1 + searchRouteToMouse(coordinates)
+    }
+    if (moveLeft) {
+      coordinates.cat[1]--
+      return 1 + searchRouteToMouse(coordinates)
+    }
+    if (moveRight) {
+      coordinates.cat[1]++
+      return 1 + searchRouteToMouse(coordinates)
+    }
+    
+    return 0;
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 describe("basic test to verify main states", () => {
   test("expect to caught", () => {
@@ -27,7 +106,7 @@ describe("basic test to verify main states", () => {
     expect(catMouse(map, 5)).toBe("Escaped!");
   });
 
-  test("if don't has 2 animals", () => {
+  test("if there's no cat or mouse", () => {
     const map = 
 `..C......
 .........
