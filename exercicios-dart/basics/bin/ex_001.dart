38e8ef0b950e660d399559cd337fcc074d5e84ae
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-10-06 23:18:27 -0300

    primeiro projeto em dart

---
+++ exercicios-dart/basics/bin/ex_001.dart
@@ -0,0 +1,8 @@
+// import 'package:ex_001/ex_001.dart' as ex_001;
+
+void main(List<String> arguments) {
+  var list2 = ["a", "b", "c"];
+  list2.add("d");
+  
+ 
+}
---
+++ exercicios-dart/basics/test/ex_001_test.dart
@@ -0,0 +1,8 @@
+import 'package:ex_001/ex_001.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
