5181997b138a66bb0728b3ae86be9c7efa85b557
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-10-18 21:33:41 -0300

    aprendi como construir os componentes basicos de um login em flutter

--- mobile/exercicios-flutter/ex_002/lib/pages/login.dart
+++ mobile/exercicios-flutter/ex_002/lib/pages/login.dart
@@ -1,3 +1,4 @@
+import 'package:ex_002/pages/home.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -8,77 +9,119 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  var email = TextEditingController(text: "exemplo@mail.com");
+  String senha = "";
+  bool isObscure = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 0, 3, 43),
-      body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          width: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(top: 48),
-                width: 200,
-                height: 200,
-                color: Colors.amber,
-                child: const Icon(
-                  Icons.person_2,
-                  size: 175,
-                ),
-              ),
-              Container(
-                alignment: Alignment.centerLeft,
-                padding: const EdgeInsets.only(left: 12),
-                width: double.infinity,
-                margin: const EdgeInsets.only(top: 48),
-                color: Colors.cyan,
-                height: 50,
-                child: const Row(
-                  children: [
-                    Text("Email:"),
-                    Expanded(child: Text("exemplo@mail.com"))
-                  ],
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.only(left: 12),
-                alignment: Alignment.centerLeft,
-                width: double.infinity,
-                margin: const EdgeInsets.symmetric(vertical: 16),
-                color: Colors.cyan,
-                height: 50,
-                child: const Row(
-                  children: [
-                    Text("Senha:"),
-                    Expanded(child: Text("senha123")),
-                  ],
-                ),
-              ),
-              Expanded(
-                child: Column(
-                  children: [
-                    Container(
-                      alignment: Alignment.center,
-                      width: double.infinity,
-                      color: Colors.red,
-                      height: 50,
-                      child: const Text("login"),
+      backgroundColor: Colors.grey.shade50,
+      body: SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints:
+              BoxConstraints(maxHeight: MediaQuery.of(context).size.height),
+          child: SafeArea(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 48),
+                    child: Image.network(
+                        "https://storage.googleapis.com/cms-storage-bucket/a9d6ce81aee44ae017ee.png",
+                        height: 400),
+                  ),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    padding: const EdgeInsets.only(left: 12),
+                    width: double.infinity,
+                    margin: const EdgeInsets.only(top: 48),
+                    height: 50,
+                    child: TextField(
+                      decoration: const InputDecoration(
+                          hintText: "Email", prefixIcon: Icon(Icons.mail)),
+                      controller: email,
                     ),
-                    Container(
-                      alignment: Alignment.center,
-                      width: double.infinity,
-                      margin: const EdgeInsets.symmetric(vertical: 16),
-                      color: Colors.red,
-                      height: 50,
-                      child: const Text("Cadastro"),
-                    )
-                  ],
-                ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(left: 12),
+                    alignment: Alignment.centerLeft,
+                    width: double.infinity,
+                    margin: const EdgeInsets.symmetric(vertical: 16),
+                    height: 50,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          contentPadding: const EdgeInsets.only(top: 16),
+                          hintText: "Senha",
+                          prefixIcon: const Icon(Icons.lock),
+                          suffixIcon: GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  isObscure = !isObscure;
+                                });
+                              },
+                              child: Icon(isObscure
+                                  ? Icons.visibility_off
+                                  : Icons.visibility))),
+                      onChanged: (value) {
+                        senha = value;
+                      },
+                      obscureText: !isObscure,
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          width: double.infinity,
+                          child: TextButton(
+                            onPressed: () {
+                              if (email.text.trim() != "" &&
+                                  senha.trim() != "") {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                        content: Text(
+                                            "Login efetuado com sucesso!")));
+                                Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const HomePage()));
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                        content:
+                                            Text("Erro ao efetuar login")));
+                              }
+                            },
+                            style: ButtonStyle(
+                                backgroundColor: MaterialStatePropertyAll(
+                                    Colors.blue.shade400),
+                                shape: MaterialStatePropertyAll(
+                                    RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(12)))),
+                            child: Text(
+                              "login",
+                              style: TextStyle(color: Colors.grey.shade50),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.center,
+                          width: double.infinity,
+                          height: 50,
+                          child: const Text("Cadastro"),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
