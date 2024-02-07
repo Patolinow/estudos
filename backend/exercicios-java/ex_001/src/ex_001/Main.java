7f81e7a00ab808e88984dade47a3fc56f20e6cca
Patolinow <65193403+Patolinow@users.noreply.github.com>
2024-02-07 19:51:50 -0300

    primeiro exercicio java

---
+++ backend/exercicios-java/ex_001/src/ex_001/Main.java
@@ -0,0 +1,28 @@
+package ex_001;
+
+public class Main {
+
+	public static void main(String[] args) {
+	int x = 1;
+	double y = 3.14;
+	boolean z = false;
+	char symbol = '@';
+	String name = "Pato";
+	
+		System.out.print("I love dart\n" + x);
+		System.out.print(symbol);
+		System.out.print(z);
+		System.out.println("hello " + name);
+	
+		
+	// this is a comment
+	
+	/*
+	 * this
+	 * is
+	 * also
+	 * a 
+	 * comment
+	 */
+	}
+}
