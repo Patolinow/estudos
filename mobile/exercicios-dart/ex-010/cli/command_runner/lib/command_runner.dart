c41fddb283d712ef76ed91500169d584bf243606
Fabio <fabiodasn1@gmail.com>
2026-08-08 19:15:41 -0300

    feat: finished chapter 5 of dart

--- mobile/exercicios-dart/ex-010/cli/command_runner/lib/command_runner.dart
+++ mobile/exercicios-dart/ex-010/cli/command_runner/lib/command_runner.dart
@@ -3,11 +3,6 @@
 /// More dartdocs go here.
 library;
 
+export 'src/arguments.dart';
 export 'src/command_runner_base.dart';
-
-class CommandRunner {
-  /// Runs the command-line application logic with the given arguments.
-  Future<void> run(List<String> input) async {
-    print('CommandRunner received arguments: $input');
-  }
-}
+export 'src/help_command.dart';
