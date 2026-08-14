7ad975074e954726a87feaf215d3b0a005c2620d
Fabio <fabiodasn1@gmail.com>
2026-08-13 21:19:33 -0300

    feat: finished part 1 of flutter tutorial

--- mobile/exercicios-flutter/ex-004/birdle/lib/main.dart
+++ mobile/exercicios-flutter/ex-004/birdle/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'game.dart';
 void main() {
   runApp(const MainApp());
 }
@@ -12,12 +12,42 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Text('Hellow', textAlign: .center),
-          ),
+          child: Tile('A', HitType.miss), // NEW
         ),
       ),
     );
   }
 }
+
+class Tile extends StatelessWidget {
+  const Tile(this.letter, this.hitType, {super.key});
+
+  final String letter;
+  final HitType hitType;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade300),
+        color: switch (hitType) {
+          HitType.hit => Colors.green,
+          HitType.partial => Colors.yellow,
+          HitType.miss => Colors.grey,
+          _ => Colors.white,
+        },
+      ),
+      child: Center(
+        child: Text(
+          letter.toUpperCase(),
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+      ),
+    );
+  }
+}
+
+
+
